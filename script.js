5c25dd4c9bce4f2c9f8b7e437f8e183061036140
yesokaio <projectnotion2@gmail.com>
2024-08-24 00:02:15 +0200

    index

---
+++ script.js
@@ -0,0 +1,22 @@
+function countdown() {
+    const targetDate = new Date('July 4, 2025 00:00:00').getTime();
+    const now = new Date().getTime();
+    const difference = targetDate - now;
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+
+    if (difference < 0) {
+        clearInterval(interval);
+        document.getElementById('countdown').innerHTML = "Der Countdown ist abgelaufen!";
+    }
+}
+
+const interval = setInterval(countdown, 1000);
